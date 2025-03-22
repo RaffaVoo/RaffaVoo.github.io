9b6f4509fb23b016ab6eaefd1a0df838379b2a64
RaffaVoo <76709068+RaffaVoo@users.noreply.github.com>
2025-03-22 14:17:16 -0500

    Try to modify blog collection structure

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,9 @@
 import { defineCollection, z } from "astro:content";
 
+// TODO: Will probably want to add loader to make it work with CMS
 const blog = defineCollection({
   type: "content",
+  loader: glob({ pattern: ["*.mdx", "*.astro"], base: "./src/content/blog"}),
   schema: z.object({
     title: z.string(),
     date: z.coerce.date(),
@@ -26,17 +28,4 @@ const projects = defineCollection({
   }),
 });
 
-const talks = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    date: z.coerce.date(),
-    lastUpdateDate: z.coerce.date().optional(),
-    description: z.string().optional(),
-    ogImage: z.string().optional(),
-    draft: z.boolean().optional(),
-    tags: z.array(z.string()).optional(),
-  }),
-});
-
-export const collections = { blog, projects, talks };
+export const collections = { blog, projects };
