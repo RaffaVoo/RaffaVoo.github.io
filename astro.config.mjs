eac310939f942864466d46c2f25f4602a96bc10e
RaffaVoo <76709068+RaffaVoo@users.noreply.github.com>
2025-03-22 14:56:05 -0500

    tweak config to hopefully resolve css issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://raffavoo.github.io",
-  base: "/raffavoo.github.io/",
+  base: "/",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
